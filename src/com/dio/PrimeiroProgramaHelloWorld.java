a7ee0f5c3353e23f13c2c85e0cc5557444427dca
isa-araujo <raraujo.isabella@gmail.com>
2022-07-19 22:09:34 -0300

    first commit

---
+++ src/com/dio/PrimeiroProgramaHelloWorld.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+public class PrimeiroProgramaHelloWorld {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
